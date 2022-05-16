98bb54f969999dea88fd3f0ffc3691c989ebc3a4
KHN <hana96024@naver.com>
2022-05-16 17:11:03 +0900

    KHN Class Added

--- src/main/java/controllers/Controllers.java
+++ src/main/java/controllers/Controllers.java
@@ -9,22 +9,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("*.student")
 public class Controllers extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String uri = request.getRequestURI();
-		
-		if(uri.equals("/delete.student")) {
-			int id = Integer.parseInt(request.getParameter("id"));
-		}
-		
-		
-		else if(uri.equals("/search.student")) {
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String uri = request.getRequestURI();
+      
+      if(uri.equals("/delete.student")) {
+         int id = Integer.parseInt(request.getParameter("id"));
+      }
+      
+      
+      else if(uri.equals("/search.student")) {
+         String name = request.getParameter("name");
+         
+      }else if(uri.equals("/add.student")){
 			String name = request.getParameter("name");
-		}
-	}
+			int kor = Integer.parseInt(request.getParameter("kor"));
+			int eng = Integer.parseInt(request.getParameter("eng"));
+			int math = Integer.parseInt(request.getParameter("math"));
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+			//int result = dao.insert(new StudentDTO(0,name,kor,eng,math,null));
+			
+			response.sendRedirect("index.jsp");
+			
+		}
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 
 }
